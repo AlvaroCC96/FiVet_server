d63bcb5690f4cba3839d5f5a5bf886c51907b556
Álvaro Lucas Castillo Calabacero <alvarolucascc96@gmail.com>
2020-05-17 20:44:59 -0400

    feat(Program.cs): implement sv in C#

--- Program.cs
+++ Program.cs
@@ -1,12 +1,45 @@
 ﻿using System;
 
-namespace fivet_server
+namespace Fivet.ZeroIce
 {
     class Program
-    {
-        static void Main(string[] args)
+    {/*
+ * Created on Sun May 17 2020
+ *
+ * Copyright (c) 2020 Your Company
+ */
+
+        public static int Main(string[] args)
+        {   /**
+            * Server waiting connections 
+            */
+            try
+            {
+                using(Ice.Communicator communicator = Ice.Util.initialize(ref args))
+                {   
+                    //TODO: testing connections , failed with Java Client
+                    var adapter = communicator.createObjectAdapterWithEndpoints("TheAdapter","default -p 8080 -z");
+                    adapter.add(new TheSystemImpl(), Ice.Util.stringToIdentity("TheSystem"));
+                    adapter.activate();
+                    communicator.waitForShutdown();
+                }
+            }
+            catch(Exception e)
+            {
+                Console.Error.WriteLine(e);
+                return 1;
+            }
+            return 0;
+        }
+    }
+
+    /**
+    * Implementation of Interface TheSystem
+    */
+    class TheSystemImpl : model.TheSystemDisp_ {
+         public override long getDelay(long clientTime, Ice.Current current)
         {
-            Console.WriteLine("Hello World!");
+            return DateTime.Now.Ticks - clientTime;
         }
     }
 }
